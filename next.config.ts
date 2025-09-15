65ee2ade2591535df9f1740f803ba976ade3e545
unknown <sulmanmansha56@gmail.com>
2025-09-15 12:16:09 +0500

    Remove next.config.mjs and update configs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  distDir: "out",
+  skipTrailingSlashRedirect: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
